fe6638d2d69b66bd410ef3218d9c4ef18c386fc5
“Mat-Techno” <matteo.courquin@gmail.com>
2021-10-05 21:48:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// Déclarer une variable
+var maVariable = 'Ma variable est déclarée';
+
+// Déclarer une constante 
+const maConstante = 'Ma constante est déclarée';
+
+// Créer une fonction
+function maFonction(){
+    console.log('Ma fonction est crée');
+}
+
+//  Le class toggle
+titre.classlist.add('taille-txt');
+// variable | ajouter la class | "taille-txt"
+
+
+// Les événemens
+btn.addEventListener('click', maFonction)
+// variable | a l'écoute d'un événement | (au clique | je lance ma fonction)
+// différents évenements :
+// 'mouseenter' quand la souris survol l'élément
+// 'keydown' quand un appuie sur une touche
+
+
+
